9ef75a941f3a4dc8cd1b7b01f7b64a60ab58e90c
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-01-21 12:24:57 +0100

    Git project initialized. POM.xml configured.

---
+++ src/main/java/com/safetynet/safetynetAlerts/SafetynetAlertsApplication.java
@@ -0,0 +1,14 @@
+package com.safetynet.safetynetAlerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tinylog.Logger;
+
+@SpringBootApplication
+public class SafetynetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/safetynetAlerts/SafetynetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
